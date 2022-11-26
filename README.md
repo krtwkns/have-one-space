9ab746c55e9d3176f0a013dbf684c084ef809103
kretawiweka.sani <kretawiweka.sani@gojek.com>
2022-11-27 01:29:46 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Have One Space
+
+## Getting started
+
+`git clone https://github.com/kretawiweka/have-one-space`
+
+Then to set up the repo:
+
+```bash
+cd have-one-space
+yarn
+yarn start
+```
+
+Site is now running at `https://localhost:3000`. Start making changes to the source code and see the site update live!
---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'Calculate Cosine Similarity',
+    description: `Calculate Cosine Similarity is a package for calculate similarity between two arrays.`,
+    imgSrc: '/static/images/npm.png',
+    href: 'https://www.npmjs.com/package/calculate-cosine-similarity',
+  },
+]
+
+export default projectsData
