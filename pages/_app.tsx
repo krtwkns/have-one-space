35efebd5249c1c6856f0269b2688928997f26912
Kretawiweka Nuraga Sani <kretawiweka.sani@gojek.com>
2023-02-21 10:12:33 +0700

    add GA id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import 'katex/dist/katex.css'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { GoogleAnalytics } from 'nextjs-google-analytics'
 
 import siteMetadata from '@/data/siteMetadata'
 import { Analytics } from 'pliny/analytics'
@@ -15,16 +16,18 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      <Analytics analyticsConfig={siteMetadata.analytics} />
-      <LayoutWrapper>
-        <SearchProvider searchConfig={siteMetadata.search}>
-          <Component {...pageProps} />
-        </SearchProvider>
-      </LayoutWrapper>
-    </ThemeProvider>
+    <>
+      <GoogleAnalytics trackPageViews />
+      <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        <LayoutWrapper>
+          <SearchProvider searchConfig={siteMetadata.search}>
+            <Component {...pageProps} />
+          </SearchProvider>
+        </LayoutWrapper>
+      </ThemeProvider>
+    </>
   )
 }
